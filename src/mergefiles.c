79cd5d85929a75e1e7b678d28b143661012ba530
Pascal Arlt <parlt@suse.com>
2019-07-15 16:14:34 +0200

    Change license from GPL-2.0 to LGPL-2.1
    Fixes: #3
    Signed-off-by: Pascal Arlt <parlt@suse.com>

--- src/mergefiles.c
+++ src/mergefiles.c
@@ -2,18 +2,21 @@
   Author: Pascal Arlt <parlt@suse.com>
   Copyright (C) 2019 SUSE Linux GmbH
 
-  This program is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 2 of the License, or
-  (at your option) any later version.
+  Licensed under the GNU Lesser General Public License Version 2.1
 
-  This program is distributed in the hope that it will be useful,
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  Lesser General Public License for more details.
 
-  You should have received a copy of the GNU General Public License
-  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, see
+  <http://www.gnu.org/licenses/>.
 */
 
 #include "../include/mergefiles.h"
