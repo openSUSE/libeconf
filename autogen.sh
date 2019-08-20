a327d26e8943189606156eeaf7f94515c7b4ff91
Thorsten Kukuk <kukuk@suse.com>
2019-08-20 13:53:42 +0200

    Add autogen.sh, which calls autoconf/automake/libtool in the correct order to create the configure script

---
+++ autogen.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -x
+
+rm -fv ltmain.sh config.sub config.guess config.h.in aclocal.m4
+aclocal -I m4
+#autoheader
+libtoolize --automake --copy
+automake --add-missing --copy --force
+autoreconf
+chmod 755 configure
