86444cf006dc49d359950bced5435382f3372e07
Thorsten Kukuk <kukuk@suse.com>
2019-08-20 16:28:13 +0200

    Add more test cases for login.defs and wrong arguments

---
+++ tests/tst-arguments2.c
@@ -0,0 +1,34 @@
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#include <stdio.h>
+#include <string.h>
+
+#include "libeconf.h"
+
+/* Test case:
+ * Provide a NULL pointer for the group. Means there is no group.
+*/
+
+int
+main(int argc, char **argv)
+{
+  Key_File *key_file;
+  char *val;
+
+  key_file = econf_get_key_file ("tst-arguments/etc/arguments.conf", " =", '#');
+  if (key_file == NULL)
+    return 1;
+
+  val = econf_getStringValue (key_file, NULL, "KEY");
+  if (val == NULL || strlen(val) == 0)
+    {
+      fprintf (stderr, "KEY returns nothing!\n");
+      return 1;
+    }
+
+  econf_destroy (key_file);
+
+  return 0;
+}
