552f78264854f26fb04303b0547cf1895fd5dad8
Thorsten Kukuk <kukuk@suse.com>
2019-08-26 13:34:12 +0200

    Add --enable-compiler-checks option to configure, enables AddressSanitizer

--- autogen.sh
+++ autogen.sh
@@ -5,6 +5,8 @@ mkdir -p m4
 aclocal -I m4
 #autoheader
 libtoolize --automake --copy
+# Fix ltmain.sh for -fsanitize
+sed -i -e 's/-stdlib=\*)/-stdlib=\*|-fsanitize=\*)/g' ltmain.sh
 automake --add-missing --copy --force
 autoreconf
 chmod 755 configure
