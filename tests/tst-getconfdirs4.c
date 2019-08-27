5c925818d1a018716bc9ac5fe48341e7b7143823
Thorsten Kukuk <kukuk@suse.com>
2019-08-27 14:27:10 +0200

    tst-getconfdirs4: make sure, only a file in /etc/ and not in /usr/etc works, too.

---
+++ tests/tst-getconfdirs4.c
@@ -0,0 +1,78 @@
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#include <stdio.h>
+#include <string.h>
+
+#include "libeconf.h"
+
+/* Test case:
+   /etc/getconfdir.conf exists
+   No configuration files in /usr/etc exists
+
+   This is to make migration easier, if the same config file is
+   read by different projects.
+*/
+
+int
+check_key(Key_File *key_file, char *key, char *expected_val)
+{
+  econf_err error = ECONF_SUCCESS;
+  char *val = econf_getStringValue (key_file, "", key, &error);
+  if (expected_val == NULL)
+    {
+      if (val == NULL)
+        return 0;
+
+      fprintf (stderr, "ERROR: %s has value \"%s\"\n", key, val);
+      return 1;
+    }
+  if (val == NULL || strlen(val) == 0)
+    {
+      fprintf (stderr, "ERROR: %s returns nothing! (%s)\n", key,
+               econf_errString(error));
+      return 1;
+    }
+  if (strcmp (val, expected_val) != 0)
+    {
+      fprintf (stderr, "ERROR: %s is not \"%s\"\n", key, expected_val);
+      return 1;
+    }
+
+  printf("Ok: %s=%s\n", key, val);
+  free (val);
+  return 0;
+}
+
+int
+main(int argc, char **argv)
+{
+  Key_File *key_file;
+  int retval = 0;
+  econf_err error;
+
+  key_file = econf_get_conf_from_dirs (
+				       TESTSDIR"tst-getconfdirs4-data/usr/etc",
+				       TESTSDIR"tst-getconfdirs4-data/etc",
+				       "getconfdir", ".conf", "=", '#', &error);
+  if (key_file == NULL)
+    {
+      fprintf (stderr, "ERROR: econf_get_conf_from_dirs returned NULL: %s\n",
+	       econf_errString(error));
+      return 1;
+    }
+
+  if (check_key(key_file, "KEY1", "etc") != 0)
+    retval = 1;
+  if (check_key(key_file, "ETC", "true") != 0)
+    retval = 1;
+  if (check_key(key_file, "USRETC", NULL) != 0)
+    retval = 1;
+  if (check_key(key_file, "OVERRIDE", NULL) != 0)
+    retval = 1;
+
+  econf_destroy (key_file);
+
+  return retval;
+}
