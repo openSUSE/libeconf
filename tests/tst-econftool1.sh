d0cc54b198c406c7423a105291483ccbb77672de
Jorik Cronenberg <jcronenberg@suse.de>
2020-08-30 11:43:40 +0200

    Update build and testcases
    Update the build system to use cmake and meson
    Update testcases to work with the new build systems

--- tests/tst-econftool1.sh
+++ tests/tst-econftool1.sh
@@ -19,10 +19,19 @@ declare -a experr=("Usage: econftool"
 
 teststringslength=${#teststrings[@]}
 
+econftool_exe="$PWD/../util/econftool"
+if ! [[ -f "$econftool_exe" ]]; then
+    econftool_exe="$PWD/econftool"
+    if ! [[ -f "$econftool_exe" ]]; then
+        echo "Couldn't find the econftool executable"
+        exit 1
+    fi
+fi
+
 got_error=false
 
 for ((i=0; i<${teststringslength}; i++)); do
-    error=$($PWD/../util/econftool ${teststrings[$i]} 2>&1)
+    error=$($econftool_exe ${teststrings[$i]} 2>&1)
     if [[ ! $error =~ ${experr[$i]} ]]; then
         echo error for: ${teststrings[$i]}
         echo expected to contain: ${experr[$i]}
--- tests/tst-econftool_show1.sh
+++ tests/tst-econftool_show1.sh
@@ -13,18 +13,28 @@ declare -a experr=("variable = 301"
 
 teststringslength=${#teststrings[@]}
 
-libeconfdir=$PWD
-if [[ $PWD =~ "_build" ]]; then
-	cd ../../..
-	export ECONFTOOL_ROOT=$PWD/tests/tst-econftool-data
-else
-	export ECONFTOOL_ROOT=$PWD/tst-econftool-data
+econftool_exe="$PWD/../util/econftool"
+if ! [[ -f "$econftool_exe" ]]; then
+    econftool_exe="$PWD/econftool"
+    if ! [[ -f "$econftool_exe" ]]; then
+        echo "Couldn't find the econftool executable"
+        exit 1
+    fi
 fi
+econftool_root="$PWD/../../tests/tst-econftool-data"
+if ! [[ -d "$econftool_root" ]]; then
+    econftool_root="$PWD/../tests/tst-econftool-data"
+    if ! [[ -d "$econftool_root" ]]; then
+        echo "Couldn't set ECONFTOOL_ROOT"
+        exit 1
+    fi
+fi
+export ECONFTOOL_ROOT=$econftool_root
 
 got_error=false
 
 for ((i=0; i<${teststringslength}; i++)); do
-    error=$($libeconfdir/../util/econftool show ${teststrings[$i]} 2>&1)
+    error=$($econftool_exe show ${teststrings[$i]} 2>&1)
     if [[ ! $error =~ ${experr[$i]} ]]; then
         echo error for ${teststrings[$i]}
         echo expected to contain: ${experr[$i]}
