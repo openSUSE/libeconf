e25c9352b14300cd0b31d439eec1a25ab26c7630
Stefan Schubert <schubi@suse.de>
2021-03-24 13:00:44 +0100

    added docu

---
+++ doc/html/dir_d44c64559bbebec7f509842c48db8b23.js
@@ -0,0 +1,5 @@
+var dir_d44c64559bbebec7f509842c48db8b23 =
+[
+    [ "libeconf.h", "libeconf_8h.html", "libeconf_8h" ],
+    [ "libeconf_ext.h", "libeconf__ext_8h.html", "libeconf__ext_8h" ]
+];
---
+++ doc/html/libeconf__ext_8h.js
@@ -0,0 +1,7 @@
+var libeconf__ext_8h =
+[
+    [ "econf_ext_value", "structeconf__ext__value.html", "structeconf__ext__value" ],
+    [ "econf_ext_value", "libeconf__ext_8h.html#a2947e32c20151f36458db1008c415989", null ],
+    [ "econf_getExtValue", "libeconf__ext_8h.html#aef2b0568b88fa788a7870a3b2bdd28a7", null ],
+    [ "econf_freeExtValue", "libeconf__ext_8h.html#a288b413a3d769a1af204c78aa11d185f", null ]
+];
