7ea7a19dfda6df53727af8570127967846bb658d
Thorsten Kukuk <kukuk@suse.com>
2025-04-08 10:46:02 +0200

    Add test case for comment character inside comment

---
+++ tests/tst-parseconfig2.c
@@ -0,0 +1,45 @@
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#include <stdio.h>
+#include <string.h>
+
+#include "libeconf.h"
+
+/* Test case:
+   Open file comment sign in comment line
+*/
+
+int
+main(void)
+{
+  __attribute__ ((__cleanup__(econf_freeFilep))) econf_file *key_file = NULL;
+  char *val = NULL;
+  econf_err error;
+
+  if ((error = econf_readFile (&key_file, TESTSDIR"tst-parseconfig-data/osrelease.conf", "=", "#")))
+    {
+      fprintf (stderr, "ERROR: couldn't read configuration file: %s\n", econf_errString(error));
+      return 1;
+    }
+
+
+  if ((error = econf_getStringValue(key_file, NULL, "ANSI_COLOR", &val)))
+    {
+      fprintf(stderr, "ERROR: couldn't get key 'ANSI_COLOR': %s\n", econf_errString(error));
+      return 1;
+    }
+
+  if (val[strlen(val)-1] == '\n')
+    {
+      fprintf(stderr, "ERROR: ANSI_COLOR has extra '\\n'!\n");
+      free(val);
+      return 77; /* XXX skip for now until fixed */
+    }
+  free (val);
+
+  key_file = econf_free (key_file);
+
+  return 0;
+}
