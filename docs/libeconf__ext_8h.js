439edc06cd92f6a22f69cdfc3acef0b7bc6684a7
Stefan Schubert <schubi@suse.de>
2025-11-25 15:35:13 +0100

    updated docu

--- docs/libeconf__ext_8h.js
+++ docs/libeconf__ext_8h.js
@@ -3,5 +3,6 @@ var libeconf__ext_8h =
     [ "econf_ext_value", "structeconf__ext__value.html", "structeconf__ext__value" ],
     [ "econf_ext_value", "libeconf__ext_8h.html#a2947e32c20151f36458db1008c415989", null ],
     [ "econf_getExtValue", "libeconf__ext_8h.html#aef2b0568b88fa788a7870a3b2bdd28a7", null ],
+    [ "econf_setExtValue", "libeconf__ext_8h.html#afd864f3b3732c1a072898b937aaebcd8", null ],
     [ "econf_freeExtValue", "libeconf__ext_8h.html#a288b413a3d769a1af204c78aa11d185f", null ]
 ];
